c90b4e7469db11285d1e16b7cb7f25e4c16281d4
Yuri Garcia <78678809+GarciaY@users.noreply.github.com>
2022-09-01 19:16:46 +0800

    Initial commit

---
+++ js/countdown.js
@@ -0,0 +1,18 @@
+var minute = 29;
+var second = 60;
+
+setInterval(function(){
+    if (minute == 0 && second == 1){
+        document.getElementById("counter").innerHTML = "00:00";
+    }else{
+        second--;
+        if (second == 0 ){
+            minute--;
+            second = 60;
+            if( minute == 0 ){
+                minute = minute;
+            }
+        }
+        document.getElementById("counter").innerHTML = minute + ":" + second;
+    }
+}, 1000)
